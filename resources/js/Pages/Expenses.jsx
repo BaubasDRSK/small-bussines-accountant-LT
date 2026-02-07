563bde5cb16acc3e2f4a570932c5adacb96ec3d5
Povilas Gr <127085633+BaubasDRSK@users.noreply.github.com>
2026-02-07 11:58:13 +0200

    expense initial sort and attachment icon added

--- resources/js/Pages/Expenses.jsx
+++ resources/js/Pages/Expenses.jsx
@@ -6,7 +6,7 @@ import Messages from './components/messages';
 import { v4 as uuidv4 } from 'uuid';
 import ModalYesCancel from './components/modalYesCancel';
 import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/24/solid'; // Assuming you have Heroicons installed
-import { data } from "autoprefixer";
+
 
 export default function Settings({ auth, newlist, updateExpenseRoute }) {
 
@@ -15,7 +15,7 @@ export default function Settings({ auth, newlist, updateExpenseRoute }) {
     const [expensesList, setExpensesList] = useState(null);
     const [timer1, setTimer1] = useState(null);
     const [pagination, setPagination] = useState(localStorage.getItem('pagination') ?? 15);
-    const [sort, setSort] = useState({ sortDirection: 'asc', sortName: 'due' });
+    const [sort, setSort] = useState({ sortDirection: 'desc', sortName: 'expense_date' });
 
     const [modalStatus, setModalStatus] = useState(false);
     const [modalItem, setModalItem] = useState('');
@@ -63,7 +63,7 @@ export default function Settings({ auth, newlist, updateExpenseRoute }) {
             switch (s.sortDirection) {
                 case 'desc': return { sortDirection: 'asc', sortName: n };
                 case 'asc': return { sortDirection: 'desc', sortName: n };
-                default: return { sortDirection: 'asc', sortName: 'due' };
+                default: return { sortDirection: 'asc', sortName: 'expense_date' };
             };
         });
     }
@@ -216,7 +216,7 @@ export default function Settings({ auth, newlist, updateExpenseRoute }) {
                                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                     <tr>
                                         {/* Sortable Column Headers */}
-                                        <th scope="col" className="px-6 py-3 cursor-pointer select-none hover:bg-gray-100 dark:hover:bg-gray-600/70 transition duration-150" onClick={() => doSort('expense_number')}>
+                                        <th scope="col" className="px-6 py-3 cursor-pointer select-none hover:bg-gray-100 dark:hover:bg-gray-600/70 transition duration-150" onClick={() => doSort('expense_date')}>
                                             <div className="flex items-center">
                                                 <div className="flex flex-col">
                                                     <p className="font-semibold">Expense number</p>
@@ -295,9 +295,18 @@ export default function Settings({ auth, newlist, updateExpenseRoute }) {
                                                 </div>
                                             </td>
 
-                                            <td className="px-6 py-4">
-                                                <p className="text-gray-900 dark:text-white">{item.name}</p>
-                                                <p className="text-xs text-gray-600 dark:text-gray-400">{item.customer.name}</p>
+                                            <td className="px-6 py-4 flex justify-between">
+                                                <div>
+                                                    <p className="text-gray-900 dark:text-white">{item.name}</p>
+                                                    <p className="text-xs text-gray-600 dark:text-gray-400">{item.customer.name}</p>
+                                                </div>
+                                                {item.attachment_path ? (
+                                                    <div>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+                                                            <path fillRule="evenodd" d="M15.621 4.379a3 3 0 0 0-4.242 0l-7 7a3 3 0 0 0 4.241 4.243h.001l.497-.5a.75.75 0 0 1 1.064 1.057l-.498.501-.002.002a4.5 4.5 0 0 1-6.364-6.364l7-7a4.5 4.5 0 0 1 6.368 6.36l-3.455 3.553A2.625 2.625 0 1 1 9.52 9.52l3.45-3.451a.75.75 0 1 1 1.061 1.06l-3.45 3.451a1.125 1.125 0 0 0 1.587 1.595l3.454-3.553a3 3 0 0 0 0-4.242Z" clipRule="evenodd" />
+                                                        </svg>
+                                                    </div>
+                                                    ) : null}
                                             </td>
 
                                             <td className="px-6 py-4 hidden lg:table-cell font-semibold text-gray-900 dark:text-white">
